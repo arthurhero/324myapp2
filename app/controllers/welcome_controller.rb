ad4b9db62e5145a5d5e70979aee50601295498f7
arthurhero <chenziwe@grinnell.edu>
2020-02-09 16:59:10 -0600

    hw3

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def about 
+  end
+
+  def story 
+  end
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -6,4 +6,13 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get about" do
+    get welcome_about_url
+    assert_response :success
+  end
+
+  test "should get story" do
+    get welcome_story_url
+    assert_response :success
+  end
 end
