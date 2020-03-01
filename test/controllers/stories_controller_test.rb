7bb1fa0b7a9d2ff3657dc8224b86663b7ffe3cfb
arthurhero <chenziwe@grinnell.edu>
2020-02-29 18:14:51 -0600

    added test for story model

--- test/controllers/stories_controller_test.rb
+++ test/controllers/stories_controller_test.rb
@@ -10,4 +10,20 @@ class StoriesControllerTest < ActionDispatch::IntegrationTest
         end
     end
 
+    test "should create and destroy a story" do
+        get stories_url
+        stories=assigns[:stories]
+        length=stories.length()
+        post stories_url, params:{ "story":{"author":'ZZZ', "text": 'Once upon a time, there was a hill. On the hill, there was a temple. In the temple, there was an old monk and a young monk. The old monk was telling the young monk a story...' } }
+        get stories_url
+        stories=assigns[:stories]
+        length2=stories.length()
+        assert length2==length+1
+        id=stories.last.id.to_s
+        delete stories_url+'/'+id
+        get stories_url
+        stories=assigns[:stories]
+        length3=stories.length()
+        assert length3==length
+    end
 end
--- test/models/story_test.rb
+++ test/models/story_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class StoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid story' do
+    story = Story.new(author: 'ZZZ', story: 'Once upon a time, there was a hill. On the hill, there was a temple. In the temple, there was an old monk and a young monk. The old monk was telling the young monk a story...')
+    assert story.valid?
+  end
+
+  test 'invalid without author' do
+    story = Story.new(story: 'Once upon a time, there was a hill. On the hill, there was a temple. In the temple, there was an old monk and a young monk. The old monk was telling the young monk a story...')
+    refute story.valid?, 'story is valid without an author'
+    assert_not_nil story.errors[:author], 'no validation error for author'
+  end
+
+  test 'invalid without text' do
+    story = Story.new(author: 'ZZZ') 
+    refute story.valid?, 'story is valid without content'
+    assert_not_nil story.errors[:story], 'no validation error for content'
+  end
 end
